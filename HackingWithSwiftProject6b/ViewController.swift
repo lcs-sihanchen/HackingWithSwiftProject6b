50bf502fffeff0d33f73b2729118ef88875a1a0e
Chen Sihan <schen@lcs.on.ca>
2020-03-24 18:00:46 -0400

    Initial Commit

---
+++ HackingWithSwiftProject6b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackingWithSwiftProject6b
+//
+//  Created by Chen, Sihan on 2020-03-24.
+//  Copyright © 2020 Chen, Sihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
